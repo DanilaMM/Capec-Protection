ed459bc338971bc78dd95d63acd28b325d3de244
DanilaMM <vip.masychev@mail.ru>
2024-01-28 18:11:41 +0300

    test models commit

--- MyDiplomSite/mysite/users/models.py
+++ MyDiplomSite/mysite/users/models.py
@@ -1,1 +1,6 @@
 from django.db import models
+
+# class Users(models.Model):
+#     login = models.CharField(unique=True, db_index=True)
+#     email = models.EmailField()
+#     password = models.CharField()
