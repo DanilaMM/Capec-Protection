173526db34bc11fe10864b79e23038e70a32d49b
DanilaMM <vip.masychev@mail.ru>
2024-01-31 19:49:39 +0300

    Изменил view на классы,полностью рабочая авторизация,выход с акка

--- MyDiplomProject/profils/urls.py
+++ MyDiplomProject/profils/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
-from .views import Render_Main, Render_Login
+from .views import Render_Main, UserLoginView, UserLogoutView, Render_glavn
 
 app_name = 'profils'
 
 urlpatterns = [
     path('main/', Render_Main, name='main'),
-    path('logun_users/', Render_Login, name='logun_users'),
+    path('logun_users/', UserLoginView.as_view(), name='logun_users'),
+    path('logout_users/', UserLogoutView.as_view(), name='logout_users'),
+    path('glavn_str/', Render_glavn, name='rend_glavn'),
 
 
 ]
--- MyDiplomProject/profils/views.py
+++ MyDiplomProject/profils/views.py
@@ -1,26 +1,38 @@
+from django.contrib.auth.decorators import login_required
 from django.http import HttpRequest
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import path
+from django.views import View
+
 from . import views
-from django.contrib.auth import views as auth_views, login, authenticate
+from django.contrib.auth import views as auth_views, login, authenticate, logout
 
 
 def Render_Main(request):
     return render(request, 'profils/main.html')
 
+@login_required(login_url='profils:logun_users')
+def Render_glavn(request):
+    return render(request, 'profils/glavn_str.html')
 
-def Render_Login(request: HttpRequest):
-    if request.method == 'POST':
-        form = request.POST
-        user = authenticate(username=form['login'], password=form['password'])
+
+class UserLoginView(View):
+
+    def post(self, request):
+        user = authenticate(username=request.POST['login'], password=request.POST['password'])
         if user:
             login(request, user)
-            return render(request,'profils/glavn_str.html')
-        else:
-            return render(request, 'profils/logun_users.html')
-
-    else:
+            return redirect('profils:rend_glavn')
         return render(request, 'profils/logun_users.html')
+
+    def get(self, request):
+        return render(request, 'profils/logun_users.html')
+class UserLogoutView(View):
+    def get(self, request):
+        logout(request)
+        return redirect('profils:main')
+
+
 
 
 
