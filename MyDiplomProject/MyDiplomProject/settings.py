c32a1686114a9161993d0a377378a7af1fcd68f6
DanilaMM <vip.masychev@mail.ru>
2024-01-31 20:51:01 +0300

    New models

--- MyDiplomProject/MyDiplomProject/settings.py
+++ MyDiplomProject/MyDiplomProject/settings.py
@@ -121,6 +121,7 @@ STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
+AUTH_USER_MODEL = 'profils.User'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
--- MyDiplomProject/profils/models.py
+++ MyDiplomProject/profils/models.py
@@ -1,3 +1,7 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
+class User(AbstractUser):
+    mail = models.EmailField(unique=True, blank=True, null=True)
+    number = models.CharField(max_length=12,blank=True, null=True)
