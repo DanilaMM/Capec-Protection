e45619b13642f230365974c69ac808e3ab67379b
DanilaMM <vip.masychev@mail.ru>
2024-01-31 14:38:46 +0300

    Замена всего, хорошего

--- MyDiplomSite/mysite/profile/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-# class Users(models.Model):
-#     login = models.CharField(unique=True, db_index=True)
-#     email = models.EmailField()
-#     password = models.CharField()
